ac16a2d74a5c165aecd9b27413857e759d6e3eb4
Ngabgna <Ngabgna@BLACKJUKER>
2023-08-21 23:36:09 +0200

    Ajoutez des fichiers projet.

---
+++ yoloV1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace yoloV1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.typeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.confidenceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.xDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.widthDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.heightDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.yoloItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnDetect = new System.Windows.Forms.Button();
+            this.txtClassName = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pic
+            // 
+            this.pic.BackColor = System.Drawing.Color.White;
+            this.pic.Location = new System.Drawing.Point(12, 12);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(597, 310);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AutoGenerateColumns = false;
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.typeDataGridViewTextBoxColumn,
+            this.confidenceDataGridViewTextBoxColumn,
+            this.xDataGridViewTextBoxColumn,
+            this.yDataGridViewTextBoxColumn,
+            this.widthDataGridViewTextBoxColumn,
+            this.heightDataGridViewTextBoxColumn});
+            this.dataGridView.DataSource = this.yoloItemBindingSource;
+            this.dataGridView.Location = new System.Drawing.Point(11, 328);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(644, 150);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // typeDataGridViewTextBoxColumn
+            // 
+            this.typeDataGridViewTextBoxColumn.DataPropertyName = "Type";
+            this.typeDataGridViewTextBoxColumn.HeaderText = "Type";
+            this.typeDataGridViewTextBoxColumn.Name = "typeDataGridViewTextBoxColumn";
+            // 
+            // confidenceDataGridViewTextBoxColumn
+            // 
+            this.confidenceDataGridViewTextBoxColumn.DataPropertyName = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.HeaderText = "Confidence";
+            this.confidenceDataGridViewTextBoxColumn.Name = "confidenceDataGridViewTextBoxColumn";
+            // 
+            // xDataGridViewTextBoxColumn
+            // 
+            this.xDataGridViewTextBoxColumn.DataPropertyName = "X";
+            this.xDataGridViewTextBoxColumn.HeaderText = "X";
+            this.xDataGridViewTextBoxColumn.Name = "xDataGridViewTextBoxColumn";
+            // 
+            // yDataGridViewTextBoxColumn
+            // 
+            this.yDataGridViewTextBoxColumn.DataPropertyName = "Y";
+            this.yDataGridViewTextBoxColumn.HeaderText = "Y";
+            this.yDataGridViewTextBoxColumn.Name = "yDataGridViewTextBoxColumn";
+            // 
+            // widthDataGridViewTextBoxColumn
+            // 
+            this.widthDataGridViewTextBoxColumn.DataPropertyName = "Width";
+            this.widthDataGridViewTextBoxColumn.HeaderText = "Width";
+            this.widthDataGridViewTextBoxColumn.Name = "widthDataGridViewTextBoxColumn";
+            // 
+            // heightDataGridViewTextBoxColumn
+            // 
+            this.heightDataGridViewTextBoxColumn.DataPropertyName = "Height";
+            this.heightDataGridViewTextBoxColumn.HeaderText = "Height";
+            this.heightDataGridViewTextBoxColumn.Name = "heightDataGridViewTextBoxColumn";
+            // 
+            // yoloItemBindingSource
+            // 
+            this.yoloItemBindingSource.DataSource = typeof(Alturos.Yolo.Model.YoloItem);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(615, 12);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(123, 23);
+            this.btnOpen.TabIndex = 3;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnDetect
+            // 
+            this.btnDetect.Location = new System.Drawing.Point(615, 41);
+            this.btnDetect.Name = "btnDetect";
+            this.btnDetect.Size = new System.Drawing.Size(123, 23);
+            this.btnDetect.TabIndex = 3;
+            this.btnDetect.Text = "Detect";
+            this.btnDetect.UseVisualStyleBackColor = true;
+            this.btnDetect.Click += new System.EventHandler(this.btnDetect_Click);
+            // 
+            // txtClassName
+            // 
+            this.txtClassName.Location = new System.Drawing.Point(615, 84);
+            this.txtClassName.Name = "txtClassName";
+            this.txtClassName.Size = new System.Drawing.Size(123, 20);
+            this.txtClassName.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(628, 122);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Search all";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 486);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.txtClassName);
+            this.Controls.Add(this.btnDetect);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.pic);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yolo V1";
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.yoloItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnDetect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn typeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn confidenceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn xDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn yDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn widthDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn heightDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource yoloItemBindingSource;
+        private System.Windows.Forms.TextBox txtClassName;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ yoloV1/Form1.cs
@@ -0,0 +1,237 @@
+﻿    using Alturos.Yolo;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data; 
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+    using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows.Forms;
+
+    namespace yoloV1
+    {
+        public partial class Form1 : Form
+        {
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
+        }
+
+        public Form1()
+            {
+                InitializeComponent();
+            }
+
+            private void btnOpen_Click(object sender, EventArgs e)
+            {
+                using(OpenFileDialog ofd = new OpenFileDialog() { Filter = "All Files|*.*|PNG|*.png|JPEG|*.jpeg|JPG|*.jpg" })
+                {
+                    if(ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        pic.Image = Image.FromFile(ofd.FileName);
+                    }
+                }
+            }
+
+            private void btnDetect_Click(object sender, EventArgs e)
+            {
+            DetectAndDisplayObjects(txtClassName.Text.ToString());
+            //DetectAndDisplayObjects("cat");
+            }
+
+        //    private void DetectAndDisplayObjects(string targetClass)
+        //{
+        //    var configurationDetector = new YoloConfigurationDetector();
+        //    var config = configurationDetector.Detect(); 
+        //    using(var yoloWrapper = new YoloWrapper(config))
+        //    {
+        //        using(MemoryStream ms = new MemoryStream())
+        //        {
+        //            pic.Image.Save(ms, ImageFormat.Png);
+        //            var items = yoloWrapper.Detect(ms.ToArray());
+
+        //            if (checkBox1.Checked)
+        //            {
+        //                yoloItemBindingSource.DataSource = items;
+        //            }
+        //            else
+        //            {
+
+        //                //Filtrage des éléments découvert pour afficher uniquement l'élément rechercher
+        //                var targetItems = items.Where(item => item.Type == targetClass).ToList();
+
+        //                yoloItemBindingSource.DataSource = targetItems;
+        //            }
+        //        }
+        //    }
+        //}
+
+        // detect le centre de l'image trouver 
+        //private void DetectAndDisplayObjects(string targetClass)
+        //{
+        //    var configurationDetector = new YoloConfigurationDetector();
+        //    var config = configurationDetector.Detect();
+        //    using (var yoloWrapper = new YoloWrapper(config))
+        //    {
+        //        // Capture a screenshot of the entire desktop
+        //        Bitmap screenshot = CaptureScreen();
+
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {
+        //            screenshot.Save(ms, ImageFormat.Png);
+        //            var items = yoloWrapper.Detect(ms.ToArray());
+
+        //            // Filter the detected items to only include the specified class
+        //            var targetItems = items.Where(item => item.Type == targetClass).ToList();
+
+        //            if (targetItems.Count > 0)
+        //            {
+        //                // Calculate the position of the center of the first detected object
+        //                var firstObject = targetItems[0];
+        //                var centerX = (firstObject.X + firstObject.Width) / 2;
+        //                var centerY = (firstObject.Y + firstObject.Height) / 2;
+
+        //                // Get the dimensions of the desktop
+        //                RECT desktopRect;
+        //                GetWindowRect(IntPtr.Zero, out desktopRect);
+        //                var desktopWidth = desktopRect.Right - desktopRect.Left;
+        //                var desktopHeight = desktopRect.Bottom - desktopRect.Top;
+
+        //                // Calculate the position of the center in dimensions of the desktop
+        //                var centerXInDesktop = desktopRect.Left + centerX;
+        //                var centerYInDesktop = desktopRect.Top + centerY;
+
+        //                // Calculate the position of the center in dimensions of the screen
+        //                var screen = Screen.FromPoint(new Point(centerXInDesktop, centerYInDesktop));
+        //                var centerXInScreen = centerXInDesktop - screen.Bounds.Left;
+        //                var centerYInScreen = centerYInDesktop - screen.Bounds.Top;
+
+        //                // Now you can use centerXInScreen and centerYInScreen for further processing
+        //                MessageBox.Show(centerXInScreen.ToString() + " " + centerYInScreen.ToString());
+        //            }
+
+        //            yoloItemBindingSource.DataSource = targetItems;
+        //        }
+        //    }
+        //}
+
+        //private void DetectAndDisplayObjects(string targetClass)
+        //{
+        //    var configurationDetector = new YoloConfigurationDetector();
+        //    var config = configurationDetector.Detect();
+        //    using (var yoloWrapper = new YoloWrapper(config))
+        //    {
+        //        // Capture a screenshot of the entire desktop
+        //        Bitmap screenshot = CaptureScreen();
+
+        //        using (MemoryStream ms = new MemoryStream())
+        //        {
+        //            screenshot.Save(ms, ImageFormat.Png);
+        //            var items = yoloWrapper.Detect(ms.ToArray());
+
+        //            // Filter the detected items to only include the specified class
+        //            var targetItems = items.Where(item => item.Type == targetClass).ToList();
+
+        //            if (targetItems.Count > 0)
+        //            {
+        //                // Get the first detected object
+        //                var firstObject = targetItems[0];
+
+        //                // Calculate the position of the center of the detected object
+        //                var centerX = firstObject.X + firstObject.Width / 2;
+        //                var centerY = firstObject.Y + firstObject.Height / 2;
+
+        //                // Draw the center point on the screenshot
+        //                using (Graphics g = Graphics.FromImage(screenshot))
+        //                {
+        //                    g.FillEllipse(Brushes.Red, centerX - 2, centerY - 2, 5, 5);
+        //                }
+
+        //                // Display the modified screenshot in the picScreenshot PictureBox
+        //                pic.Image = screenshot;
+        //            }
+        //            yoloItemBindingSource.DataSource = targetItems;
+        //        }
+        //    }
+        //}
+        private void DetectAndDisplayObjects(string targetClass)
+        {
+            var configurationDetector = new YoloConfigurationDetector();
+            var config = configurationDetector.Detect();
+            using (var yoloWrapper = new YoloWrapper(config))
+            {
+                // Capture a screenshot of the entire desktop
+                Bitmap screenshot = CaptureScreen();
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    screenshot.Save(ms, ImageFormat.Png);
+                    var items = yoloWrapper.Detect(ms.ToArray());
+
+                    // Filter the detected items to only include the specified class
+                    var targetItems = items.Where(item => item.Type == targetClass).ToList();
+
+                    if (targetItems.Count > 0)
+                    {
+                        // Get the first detected object
+                        var firstObject = targetItems[0];
+
+                        // Calculate the position of the center of the detected object
+                        var centerX = firstObject.X + firstObject.Width / 2;
+                        var centerY = firstObject.Y + firstObject.Height / 2;
+
+                        // Draw the center point on the screenshot
+                        using (Graphics g = Graphics.FromImage(screenshot))
+                        {
+                            // Draw a larger red circle around the center point
+                            int circleRadius = 10;
+                            int circleDiameter = circleRadius * 2;
+                            g.DrawEllipse(Pens.Red, centerX - circleRadius, centerY - circleRadius, circleDiameter, circleDiameter);
+
+                            // Draw a smaller filled red circle at the center point
+                            int pointSize = 5;
+                            g.FillEllipse(Brushes.Red, centerX - pointSize, centerY - pointSize, pointSize * 2, pointSize * 2);
+                        }
+
+                        // Display the modified screenshot in the picScreenshot PictureBox
+                        pic.Image = screenshot;
+                    }
+
+                    yoloItemBindingSource.DataSource = targetItems;
+                }
+            }
+        }
+
+
+
+
+
+        private Bitmap CaptureScreen()
+        {
+            Rectangle screenBounds = Screen.PrimaryScreen.Bounds;
+            Bitmap screenshot = new Bitmap(screenBounds.Width, screenBounds.Height, PixelFormat.Format32bppArgb);
+
+            using (Graphics g = Graphics.FromImage(screenshot))
+            {
+                g.CopyFromScreen(screenBounds.Left, screenBounds.Top, 0, 0, screenBounds.Size, CopyPixelOperation.SourceCopy);
+            }
+
+            return screenshot;
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+    }
+    }
